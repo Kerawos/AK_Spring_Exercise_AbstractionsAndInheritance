2e676c8c412a835946bbf5c062008a1c91bf67a6
Kerawos <Maresowa@gmail.com>
2017-12-04 10:42:00 +0100

    @repplace by servise, add comments

--- src/main/java/pl/akademiakodu/AK_Spring_Exercise_AbstractionsAndInheritance/models/Inheritance.java
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_AbstractionsAndInheritance/models/Inheritance.java
@@ -1,10 +1,12 @@
 package pl.akademiakodu.AK_Spring_Exercise_AbstractionsAndInheritance.models;
 
+import org.springframework.stereotype.Service;
 import pl.akademiakodu.AK_Spring_Exercise_AbstractionsAndInheritance.models.entities.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class Inheritance {
 
     private List<Human> heroes;
